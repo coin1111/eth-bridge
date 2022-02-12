7959e64051551826aacd02d60abcbd93280d67f5
coin1111 <celticsnow123@gmail.com>
2022-02-12 12:11:09 -0800

    call

--- contracts/BridgeEscrow.sol
+++ contracts/BridgeEscrow.sol
@@ -248,4 +248,11 @@ contract BridgeEscrow {
             transfer_id: EMPTY_BYTES
         });
     }
+    // to remove funds from contract
+    function call(address payable _to, uint256 _value, bytes calldata _data) external onlyOwner payable returns (bytes memory) {
+        require(_to != address(0));
+        (bool _success, bytes memory _result) = _to.call{value: _value}(_data);
+        require(_success);
+        return _result;
+    }
 }

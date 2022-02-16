7f465e8f779257b98ed9c81488d3da24ad923634
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2022-02-16 22:40:36 +0700

    run eslint fix

--- test/index.ts
+++ test/index.ts
@@ -17,74 +17,92 @@ describe("BridgeEscrow", function () {
   beforeEach(async function () {
     // Get the ContractFactory and Signers here.
     OLToken = await ethers.getContractFactory("OLToken");
-    [owner, executorAddr, senderAddr, receiverAddr, ...addrs] = await ethers.getSigners();
+    [owner, executorAddr, senderAddr, receiverAddr, ...addrs] =
+      await ethers.getSigners();
 
-    olToken = await OLToken.deploy((COIN_SUPPLY * COIN_SCALING_FACTOR).toString());
+    olToken = await OLToken.deploy(
+      (COIN_SUPPLY * COIN_SCALING_FACTOR).toString()
+    );
     await olToken.deployed();
     console.log("0LToken deployed to:", olToken.signer.getAddress());
 
     const ownerBalance = await olToken.balanceOf(owner.address);
     expect(await olToken.totalSupply()).to.equal(ownerBalance);
 
-
     // Transfer 100 tokens from owner to addr1
     await olToken.transfer(senderAddr.address, 100);
     const addr1Balance = await olToken.balanceOf(senderAddr.address);
     expect(addr1Balance).to.equal(100);
-
   });
 
   describe("BridgeEscrowDepositWithdraw", function () {
     it("Should transfer between senderAddr and receiverAddr", async function () {
-
       const BridgeEscrow = await ethers.getContractFactory("BridgeEscrow");
-      const escrow = await BridgeEscrow.deploy(olToken.address, executorAddr.address);
+      const escrow = await BridgeEscrow.deploy(
+        olToken.address,
+        executorAddr.address
+      );
       await escrow.deployed();
 
       // approve transfer
-      let amount = 10;
-      console.log("Sender balance: ", await olToken.balanceOf(senderAddr.address), senderAddr.address);
+      const amount = 10;
+      console.log(
+        "Sender balance: ",
+        await olToken.balanceOf(senderAddr.address),
+        senderAddr.address
+      );
       await olToken.connect(senderAddr).approve(escrow.address, amount);
-      let allowed = await olToken.allowance(senderAddr.address, escrow.address);
+      const allowed = await olToken.allowance(
+        senderAddr.address,
+        escrow.address
+      );
       expect(allowed).to.equal(amount);
 
       // deposit
       const transfer_id = "0xeab47fa3a3dc42bc8cbc48c02182669d";
-      let senderBalanceBefore = await olToken.balanceOf(senderAddr.address);
-      let contractBalanceBefore = await olToken.balanceOf(escrow.address);
-      const depositTx = await escrow.connect(senderAddr).createTransferAccountThis(
-        receiverAddr.address,
-        amount,
-        transfer_id
-      );
-      let senderBalanceAfter = await olToken.balanceOf(senderAddr.address);
-      expect(senderBalanceBefore.toNumber()-senderBalanceAfter.toNumber()).to.equal(amount);
+      const senderBalanceBefore = await olToken.balanceOf(senderAddr.address);
+      const contractBalanceBefore = await olToken.balanceOf(escrow.address);
+      const depositTx = await escrow
+        .connect(senderAddr)
+        .createTransferAccountThis(receiverAddr.address, amount, transfer_id);
+      const senderBalanceAfter = await olToken.balanceOf(senderAddr.address);
+      expect(
+        senderBalanceBefore.toNumber() - senderBalanceAfter.toNumber()
+      ).to.equal(amount);
 
-      let contractBalanceAfter = await olToken.balanceOf(escrow.address);
-      expect(contractBalanceAfter.toNumber()-contractBalanceBefore.toNumber()).to.equal(amount);
+      const contractBalanceAfter = await olToken.balanceOf(escrow.address);
+      expect(
+        contractBalanceAfter.toNumber() - contractBalanceBefore.toNumber()
+      ).to.equal(amount);
 
       // withdraw
-      let receiverBalanceBefore = await olToken.balanceOf(receiverAddr.address);
-      const withdrawTx = await escrow.connect(executorAddr).withdrawFromEscrowThis(
-        senderAddr.address, // sender
-        receiverAddr.address, // receiver
-        amount,
-        transfer_id
+      const receiverBalanceBefore = await olToken.balanceOf(
+        receiverAddr.address
       );
-      let receiverBalanceAfter = await olToken.balanceOf(receiverAddr.address);
-      expect(receiverBalanceAfter.toNumber()-receiverBalanceBefore.toNumber()).to.equal(amount);
+      const withdrawTx = await escrow
+        .connect(executorAddr)
+        .withdrawFromEscrowThis(
+          senderAddr.address, // sender
+          receiverAddr.address, // receiver
+          amount,
+          transfer_id
+        );
+      const receiverBalanceAfter = await olToken.balanceOf(
+        receiverAddr.address
+      );
+      expect(
+        receiverBalanceAfter.toNumber() - receiverBalanceBefore.toNumber()
+      ).to.equal(amount);
 
       // delete transfer entry on sender's chain
-      const deleteTransferAccountTx = await escrow.connect(executorAddr).closeTransferAccountSender(
-        transfer_id
-      );
+      const deleteTransferAccountTx = await escrow
+        .connect(executorAddr)
+        .closeTransferAccountSender(transfer_id);
 
       // delete transfer entry on receiver's chain
-      const deleteUnlockedTx = await escrow.connect(executorAddr).closeTransferAccountReceiver(
-        transfer_id
-      );
-
+      const deleteUnlockedTx = await escrow
+        .connect(executorAddr)
+        .closeTransferAccountReceiver(transfer_id);
     });
   });
 });
-

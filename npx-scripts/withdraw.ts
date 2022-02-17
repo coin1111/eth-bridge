09c1f71cfd27bc31e78d7f8f02893bfc36938096
coin1111 <celticsnow123@gmail.com>
2022-02-16 18:25:52 -0800

    deposit

---
+++ npx-scripts/withdraw.ts
@@ -0,0 +1,63 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "ethers";
+import { ERC20__factory } from "../typechain/factories/ERC20__factory";
+import { BridgeEscrow__factory } from "../typechain/factories/BridgeEscrow__factory";
+import { getConfig, getSigners, getSigner } from "./get_signers";
+
+
+async function main() {
+  let argv = process.argv.slice(2);
+  if (argv.length < 3 || argv[0] == "-h" || argv[0] == "--help") {
+    console.log("Usage: withdraw.ts  sender receiver amount");
+    console.log("\t withdraw funds from escrow into receiver for amount");
+    console.log("\t sender - nick or address of depositor");
+    console.log("\t receiver - nick or address of receiver");
+    console.log("\t nicknames: alice, bob, carol, pete, todd, bridgeEscrow");
+    return;
+  }
+
+  let sender = argv[0];
+  let receiver = argv[1];
+  let amount = argv[2];
+
+  // get signers
+  let signers = getSigners();
+  let senderWallet = getSigner(signers, sender);
+  let receiverWallet = getSigner(signers, receiver);
+  let aliceWallet = getSigner(signers, "alice");
+
+  // get contracts
+  let config = getConfig();
+  let olTokenAddr = config.olTokenContract;
+  let bridgeEscrowAddr = config.escrowContract;
+  const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
+  let olToken = ERC20__factory.connect(olTokenAddr, provider);
+  const BridgeEscrow = BridgeEscrow__factory.connect(bridgeEscrowAddr, provider);
+
+  // // Deposit
+  const transfer_id = "0xeab47fa3a3dc42bc8cbc48c02182669d";
+
+  let signer = aliceWallet.connect(provider);
+  const tx = await BridgeEscrow.connect(signer).withdrawFromEscrowThis(
+    senderWallet.address, // sender
+    receiverWallet.address, // receiver
+    amount,
+    transfer_id,
+
+  );
+  console.log("Withdraw: ", tx);
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+

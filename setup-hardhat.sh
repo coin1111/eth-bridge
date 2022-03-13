03ee50703d5a8d77d953429c5e428f7dd2fb2e83
coin1111 <celticsnow123@gmail.com>
2022-03-12 19:05:09 -0800

    abi

--- setup-hardhat.sh
+++ setup-hardhat.sh
@@ -1,3 +1,5 @@
 npm install --save-dev @nomiclabs/hardhat-waffle ethereum-waffle chai @nomiclabs/hardhat-ethers ethers
 npm install @openzeppelin/contracts
+sudo npm install --global yarn
+yarn add --dev hardhat-abi-exporter
 

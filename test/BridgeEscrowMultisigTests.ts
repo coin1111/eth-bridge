e7eb2d1b7b8054a63bc10ad6106628e0b059cccf
coin1111 <celticsnow123@gmail.com>
2022-05-06 18:08:18 -0700

    multisig test

--- test/BridgeEscrowMultisigTests.ts
+++ test/BridgeEscrowMultisigTests.ts
@@ -36,8 +36,8 @@ describe("BridgeEscrowMultisig", function () {
 
   });
 
-  describe("BridgeEscrowMultisig", function () {
-    it("Should be able to deposit and withdraw", async function () {
+  describe("BridgeEscrowDeposit", function () {
+    it("Should be able to deposit", async function () {
 
       const BridgeEscrowMultisig = await ethers.getContractFactory("BridgeEscrowMultisig");
       const allowedExecutors = [executorAddr.address, executorAddr1.address, executorAddr2.address];
@@ -62,10 +62,10 @@ describe("BridgeEscrowMultisig", function () {
         transfer_id_dep
       );
       let senderBalanceAfter = await olToken.balanceOf(senderAddr.address);
-      expect(senderBalanceBefore.toNumber()-senderBalanceAfter.toNumber()).to.equal(amount);
+      expect(senderBalanceBefore.toNumber() - senderBalanceAfter.toNumber()).to.equal(amount);
 
       let contractBalanceAfter = await olToken.balanceOf(escrow.address);
-      expect(contractBalanceAfter.toNumber()-contractBalanceBefore.toNumber()).to.equal(amount);
+      expect(contractBalanceAfter.toNumber() - contractBalanceBefore.toNumber()).to.equal(amount);
 
       // account is locked
       let ai_locked = await escrow.getLockedAccountInfo(transfer_id_dep);
@@ -75,22 +75,46 @@ describe("BridgeEscrowMultisig", function () {
       const deleteTransferAccountTx = await escrow.connect(executorAddr).closeTransferAccountSender(
         transfer_id_dep
       );
-      console.log("deleteTransferAccountTx: "+deleteTransferAccountTx);
+      console.log("deleteTransferAccountTx: " + deleteTransferAccountTx);
 
 
       let ai = await escrow.getLockedAccountInfo(transfer_id_dep);
-      console.log("ai: "+ai);
+      console.log("ai: " + ai);
       expect(ai.is_closed).to.equal(false);
 
       // delete transfer entry on sender's chain using executorAddr1
       const deleteTransferAccountTx1 = await escrow.connect(executorAddr1).closeTransferAccountSender(
         transfer_id_dep
       );
-      console.log("deleteTransferAccountTx1: "+deleteTransferAccountTx1);
+      console.log("deleteTransferAccountTx1: " + deleteTransferAccountTx1);
 
       ai = await escrow.getLockedAccountInfo(transfer_id_dep);
-      console.log("ai: "+ai);
+      console.log("ai: " + ai);
       expect(ai.is_closed).to.equal(true);
+    });
+  });
+
+  describe("BridgeEscrowWithdraw", function () {
+    it("Should be able to withdraw", async function () {
+
+      const BridgeEscrowMultisig = await ethers.getContractFactory("BridgeEscrowMultisig");
+      const allowedExecutors = [executorAddr.address, executorAddr1.address, executorAddr2.address];
+      const escrow = await BridgeEscrowMultisig.deploy(olToken.address, allowedExecutors, 2);
+      await escrow.deployed();
+
+      // approve transfer
+      let amount = 10;
+      console.log("Sender balance: ", await olToken.balanceOf(senderAddr.address), senderAddr.address);
+      await olToken.connect(senderAddr).approve(escrow.address, amount);
+      let allowed = await olToken.allowance(senderAddr.address, escrow.address);
+      expect(allowed).to.equal(amount);
+
+      // fund escrow
+      let contractBalanceBefore = await olToken.balanceOf(escrow.address);
+      await olToken.connect(senderAddr).transfer(escrow.address, 100);
+
+      let contractBalanceAfter = await olToken.balanceOf(escrow.address);
+      expect(contractBalanceAfter.toNumber() - contractBalanceBefore.toNumber()).to.equal(100);
 
       // withdraw
       const transfer_id_w = "0xeab47fa3a3dc42bc8cbc48c02182669a";
@@ -104,12 +128,12 @@ describe("BridgeEscrowMultisig", function () {
         amount,
         transfer_id_w
       );
-      ai = await escrow.getUnlockedAccountInfo(transfer_id_w);
-      console.log("ai: "+ai);
+      let ai = await escrow.getUnlockedAccountInfo(transfer_id_w);
+      console.log("ai: " + ai);
       expect(ai.is_closed).to.equal(false); // transfer is pending
 
       let receiverBalanceAfter1 = await olToken.balanceOf(receiverAddr.address);
-      expect(receiverBalanceAfter1.toNumber()-receiverBalanceBefore.toNumber()).to.equal(0);
+      expect(receiverBalanceAfter1.toNumber() - receiverBalanceBefore.toNumber()).to.equal(0);
 
       // call using executorAddr1
       const withdrawTx1 = await escrow.connect(executorAddr1).withdrawFromEscrow(
@@ -119,26 +143,27 @@ describe("BridgeEscrowMultisig", function () {
         transfer_id_w
       );
       ai = await escrow.getUnlockedAccountInfo(transfer_id_w);
-      console.log("ai: "+ai);
+      console.log("ai: " + ai);
       expect(ai.is_closed).to.equal(true); // transfer is completed
 
 
       let receiverBalanceAfter = await olToken.balanceOf(receiverAddr.address);
-      expect(receiverBalanceAfter.toNumber()-receiverBalanceBefore.toNumber()).to.equal(amount);
+      expect(receiverBalanceAfter.toNumber() - receiverBalanceBefore.toNumber()).to.equal(amount);
 
 
     });
   });
 });
 
-function hexStringToByteArray(hexString:String):Uint8Array {
+
+function hexStringToByteArray(hexString: String): Uint8Array {
   if (hexString.length % 2 !== 0) {
-      throw "Must have an even number of hex digits to convert to bytes";
+    throw "Must have an even number of hex digits to convert to bytes";
   }
   var numBytes = hexString.length / 2;
   var byteArray = new Uint8Array(numBytes);
-  for (var i=0; i<numBytes; i++) {
-      byteArray[i] = parseInt(hexString.substr(i*2, 2), 16);
+  for (var i = 0; i < numBytes; i++) {
+    byteArray[i] = parseInt(hexString.substr(i * 2, 2), 16);
   }
   return byteArray;
 }

6dccbef3b984ef1da7ac2776ea171041f8a4a4c8
coin1111 <celticsnow123@gmail.com>
2022-08-21 21:24:52 -0700

    rc

--- scripts/deploy-avax.ts
+++ scripts/deploy-avax.ts
@@ -20,6 +20,7 @@ async function main() {
 
   console.log("Deploying contract with minVotes: ",minVotes);
 
+
   // Deploy ERC20 token contract
   let OLToken = await ethers.getContractFactory("OLToken");
 
@@ -27,30 +28,30 @@ async function main() {
   await olToken.deployed();
   console.log("0LToken contract:", olToken.address);
 
-//  let [alice, bob, carol, pete, todd, bridgeEscrow, ...addrs] = await ethers.getSigners();
-  let aliceAddress = "0x37d1D0AF3Cb314051FaeaFB9D0501d7e67503634";
-  let bobAddress = "0xBd11eDD68a7A1117C3eC93eA4dD5543F9Cb409CB";
-  let carolAddress = "0x39312F00Afb9c7245FE70c381a7DCf125E8173Ca";
-  let peteAddress = "0xaB2130f3f717562BFf52a1Ff78AC66404784C14d";
-  let toddAddress = "0x62a7CB8cC3aB47C410c051aB93C749488b9399e4";
-  let bridgeEscrowAddress = "0x15ef6A06cF6BF0e9668aAa357d63e17100c7060C";
+    let alice = ethers.Wallet.fromMnemonic("crack switch convince advance sea virus coast disease attack impose hint neutral")
+    let bob = ethers.Wallet.fromMnemonic("crack switch convince advance sea virus coast disease attack impose hint neutral", `m/44'/60'/0'/0/1`)
+    let carol = ethers.Wallet.fromMnemonic("crack switch convince advance sea virus coast disease attack impose hint neutral", `m/44'/60'/0'/0/2`)
+    let pete = ethers.Wallet.fromMnemonic("crack switch convince advance sea virus coast disease attack impose hint neutral", `m/44'/60'/0'/0/3`)
+    let todd = ethers.Wallet.fromMnemonic("crack switch convince advance sea virus coast disease attack impose hint neutral", `m/44'/60'/0'/0/4`)
+    let bridgeEscrow = ethers.Wallet.fromMnemonic("crack switch convince advance sea virus coast disease attack impose hint neutral", `m/44'/60'/0'/0/5`)
 
-  console.log("Alice address: ", aliceAddress);
-  console.log("Bob address: ", bobAddress);
-  console.log("Carol address: ", carolAddress);
-  console.log("pete address: ", peteAddress);
-  console.log("todd address: ", toddAddress);
+
+  console.log("Alice address: ", alice.address);
+  console.log("Bob address: ", bob.address);
+  console.log("Carol address: ", carol.address);
+  console.log("pete address: ", pete.address);
+  console.log("todd address: ", todd.address);
 
 
   // Deploy BridgeEscrow contract
   const BridgeEscrow = await ethers.getContractFactory("BridgeEscrowMultisig");
-  const escrow = await BridgeEscrow.connect(bridgeEscrowAddress)
-    .deploy(olToken.address, [aliceAddress, bobAddress], minVotes);
+  const escrow = await BridgeEscrow.connect(bridgeEscrow)
+    .deploy(olToken.address, [alice.address, bob.address], minVotes);
   await escrow.deployed();
   console.log("BridgeEscrow contract:", escrow.address);
 
   // Transfer 100 tokens from owner to pete
-  await olToken.transfer(peteAddress, 100);
+  await olToken.transfer(pete.address, 100);
 
   // Transfer 1000000 tokens from owner to escrow account
   await olToken.transfer(escrow.address, 1000000);
